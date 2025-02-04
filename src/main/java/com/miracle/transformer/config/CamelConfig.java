c20afd64f99b1749714a3ca722f2c6b33210f225
Jayasri Duggina <33040116+Jayasri1999@users.noreply.github.com>
2025-02-04 09:00:47 -0600

    Update CamelConfig.java

--- src/main/java/com/miracle/transformer/config/CamelConfig.java
+++ src/main/java/com/miracle/transformer/config/CamelConfig.java
@@ -9,9 +9,9 @@ public class CamelConfig extends RouteBuilder {
     @Override
     public void configure() {
         from("activemq:queue:inputQueue?disableReplyTo=true")
-            .log("### Received XML Message: ${body}")
-            .to("xslt:transform.xsl") // Apply XSLT transformation
-            .log("### Transformed JSON: ${body}")
-            .to("activemq:queue:outputQueue"); // Send to another queue
+            .log("Received XML Message: ${body}")
+            .to("xslt:transform.xsl")
+            .log("Transformed JSON: ${body}")
+            .to("activemq:queue:outputQueue");
     }
 }
